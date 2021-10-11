9f60aac6aa1231cf8ed4ba273c5d36959c344135
Chris Yang <kimisme9386@gmail.com>
2021-10-11 01:09:46 +0000

    fix: update integration test

--- src/integ.default.ts
+++ src/integ.default.ts
@@ -6,32 +6,68 @@ export class IntegTesting {
   constructor() {
     const app = new cdk.App();
 
+    const envUSEast1 = {
+      region: 'us-east-1',
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+    };
+
+    /**
+     * Test WAF for CloudFront
+     */
+    const stackTest1 = new cdk.Stack(app, 'TestStackAutomatedWafForCloudFront', { env: envUSEast1 });
+
+    new AutomatedWaf(stackTest1, 'AutomatedWaf', {
+      waf2Scope: Waf2ScopeOption.CLOUDFRONT,
+      resourceNamingPrefix: 'CloudFront_ApiGW',
+      errorThreshold: 55,
+      requestThreshold: 3000,
+      blockPeriod: 60,
+      logLevel: LogLevel.DEBUG,
+    });
+
     const env = {
       region: process.env.CDK_DEFAULT_REGION,
       account: process.env.CDK_DEFAULT_ACCOUNT,
     };
 
-    const stackTest1 = new cdk.Stack(app, 'TestStack1AutomatedWaf', { env });
+    /**
+     * Test For ALB
+     */
+    const stackTest2 = new cdk.Stack(app, 'TestStackAutomatedWafForALB', { env });
 
-    new AutomatedWaf(stackTest1, 'AutomatedWaf', {
-      waf2Scope: Waf2ScopeOption.REGIONAL,
-      resourceNamingPrefix: 'Alb_Api',
-      errorThreshold: 50,
-      requestThreshold: 500,
-      blockPeriod: 120,
-      logLevel: LogLevel.DEBUG,
-    });
-
-    const stackTest2 = new cdk.Stack(app, 'TestStack2AutomatedWaf', { env });
+    const albArn = `arn:aws:elasticloadbalancing:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:loadbalancer/app/ApiNe-Alb16-2VIC9075YQEZ/db92cdc88d2e7c9d`;
 
     new AutomatedWaf(stackTest2, 'AutomatedWaf', {
       waf2Scope: Waf2ScopeOption.REGIONAL,
-      resourceNamingPrefix: 'Alb_Api2',
-      errorThreshold: 60,
-      requestThreshold: 600,
-      blockPeriod: 120,
+      associatedResourceArn: albArn,
+      resourceNamingPrefix: 'Alb_Api',
+      errorThreshold: 50,
+      requestThreshold: 300,
+      blockPeriod: 60,
       logLevel: LogLevel.DEBUG,
     });
+
+
+    /**
+     * Test For API Gateway
+     */
+    const stackTest3 = new cdk.Stack(app, 'TestStackAutomatedWafForApiGW', { env });
+
+    /**
+     * Ref Stage arn in https://docs.aws.amazon.com/apigateway/latest/developerguide/arn-format-reference.html
+     */
+    const restApiArn = `arn:aws:apigateway:${cdk.Aws.REGION}::/restapis/0j90w09yf9/stages/prod`
+
+    new AutomatedWaf(stackTest3, 'AutomatedWaf', {
+      waf2Scope: Waf2ScopeOption.REGIONAL,
+      associatedResourceArn: restApiArn,
+      resourceNamingPrefix: 'ApiGW',
+      errorThreshold: 50,
+      requestThreshold: 300,
+      blockPeriod: 60,
+      logLevel: LogLevel.DEBUG,
+    });
+
 
     app.synth();
     this.stack = [stackTest1, stackTest2];
